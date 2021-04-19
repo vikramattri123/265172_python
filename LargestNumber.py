85615996f48c720f1222549e3c3fbb52ef662851
vikramattri123 <vikramattri123@gmail.com>
2021-04-20 00:15:05 +0530

    Number

---
+++ LargestNumber.py
@@ -0,0 +1,9 @@
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+c = int(input("Enter Third Number : "))
+if(a>b and a>c):
+    print(a,"Largest No")
+elif(b>a and b>c):
+    print(b," is the Largest No")
+elif(c>a and c>b):
+    print(c," is the Largest No")
