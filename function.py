fcd49c774dab59b0fac8ccbfaa4210b47b10fa14
vikramattri123 <vikramattri123@gmail.com>
2021-04-22 22:28:39 +0530

    Max & Min

---
+++ function.py
@@ -0,0 +1,16 @@
+def min(a,b):
+    if(a<=b):
+        print(a)
+    else:
+        print(b)
+
+def max(a,b):
+    if(a>=b):
+        print(a)
+    else:
+        print(b)
+
+max(22,33)
+min(7,2)
+
+
