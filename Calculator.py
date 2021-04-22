85799d5996caf619e899c2ef1ca527972e64172a
vikramattri123 <vikramattri123@gmail.com>
2021-04-22 22:44:20 +0530

    Calculator

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+
+c=a+b
+d=a-b
+e=a/b
+f=a*b
+print("The Sum of two No : ",c)
+print("The Sub of two No : ",d)
+print("The Div of two No : ",e)
+print("The Mul of two No : ",f)
