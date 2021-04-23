bba564434539e92a35fbe4901927bcb764d84918
vikramattri123 <vikramattri123@gmail.com>
2021-04-24 00:09:59 +0530

    codes

---
+++ abbervation_Check.py
@@ -0,0 +1,9 @@
+a = input("Enter Text : ")
+if a == "lol":
+    print("laughing out loud")
+elif a == "rofl":
+    print("rolling on the floor laughing")
+elif a == "lmk":
+    print("let me know")
+elif a == "smh":
+    print("shaking my head")
