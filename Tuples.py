5f1ea3c8aec6dfd4ab49c2a0ae3c0dc228a9b811
vikramattri123 <vikramattri123@gmail.com>
2021-04-27 00:14:19 +0530

    Tuple

---
+++ Tuples.py
@@ -0,0 +1,15 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+# Different types of tuples
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+print(tuple1)
+print(tuple2)
+print(tuple3)
