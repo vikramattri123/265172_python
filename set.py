7ce9c5b485916898341d35e599e608ea5ceee3a3
vikramattri123 <vikramattri123@gmail.com>
2021-04-28 21:55:03 +0530

    Set

---
+++ set.py
@@ -0,0 +1,17 @@
+s = {"bannana","cherry","Mango","Grapes","Pineapple"}
+print(s)
+
+#To find the type
+print(type(s))
+
+#to find len
+print(len(s))
+for i in s:
+    print(i)
+
+s.add("Gauva")
+print(s)
+
+s.remove("cherry")
+print(s)
+
