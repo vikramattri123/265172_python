8353449ac32077a21d5afd48545ef340146168ae
vikramattri123 <vikramattri123@gmail.com>
2021-04-29 23:31:01 +0530

    Map

--- map.py
+++ map.py
@@ -1,8 +1,15 @@
-#To update the list
-def update(x):
-    x=x*2
-    return x
+# Using Map we can find the prime no , Non-Prime Number is represented as None
+def prime(x):
+    flag=0
+    m=[]
+    for i in range(2,x):
+        if x%i == 0:
+            flag=flag+1
+        if x==2:
+            flag=0
+    if flag == 0:
+        return x
 
-nums = [12,21,32,123,34]
-s=list(map(update,nums))
+nums = [3,5,8,11]
+s=list(map(prime,nums))
 print(s)
